cc7bfabb38e9c0487618014801189580c7436da9
Baibhav <>
2021-01-31 12:31:37 -0800

    Models and config added

--- Redis-SpringBoot-App/src/main/java/com/baibhav/redisspringboot/controller/RedisController.java
+++ Redis-SpringBoot-App/src/main/java/com/baibhav/redisspringboot/controller/RedisController.java
@@ -1,14 +1,39 @@
 package com.baibhav.redisspringboot.controller;
+import com.baibhav.redisspringboot.entity.Product;
+import com.baibhav.redisspringboot.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RedisController {
 
-    @GetMapping("/hello")
-    public String checkEndPoint(){
-        return "hello world";
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping("/add")
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
     }
 
+    @GetMapping("/test")
+    public String test() {
+        return "hello redis";
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id) {
+        return dao.deleteProduct(id);
+    }
 }
